ed9d3bcb738a936b750474cc08c461f96e5e6662
Syeda Sana Zehra <76579833+sanazehra2001@users.noreply.github.com>
2024-08-16 14:56:42 +0500

    Atomicity of User Creation and Adding to Groups

--- toDoApp/signals.py
+++ toDoApp/signals.py
@@ -3,22 +3,36 @@ from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from toDoApp.models import CustomUser, Employee, Employer
 import logging
+from django.db import transaction
 
 @receiver(post_save, sender=Employee)
 def assign_employee_to_group(sender, instance, created, **kwargs):
-    logging.info(f"User created: {instance.email}, Role: {instance.role}")
+    
     if created:
-        if instance.role == CustomUser.Role.EMPLOYEE:
-            group = Group.objects.get(name='Employees')
-        
-        instance.groups.add(group)
+        try: 
+            with transaction.atomic():
+                if instance.role == CustomUser.Role.EMPLOYEE:
+                    group = Group.objects.get(name='Employees')
+                
+                instance.groups.add(group)
+                logging.info(f"User created: {instance.email}, Role: {instance.role}")
+        except Exception as e:
+            logging.error(f"Error adding user to group: {e}")
+            instance.delete()
 
 
 @receiver(post_save, sender=Employer)
 def assign_employer_to_group(sender, instance, created, **kwargs):
-    logging.info(f"User created: {instance.email}, Role: {instance.role}")
+    
     if created:
-        if instance.role == CustomUser.Role.EMPLOYER:
-            group = Group.objects.get(name='Employers')
-        
-        instance.groups.add(group)
+        try: 
+            with transaction.atomic():
+                if instance.role == CustomUser.Role.EMPLOYER:
+                    group = Group.objects.get(name='Employers')
+                
+                instance.groups.add(group)
+                logging.info(f"User created: {instance.email}, Role: {instance.role}")
+
+        except Exception as e:
+            logging.error(f"Error adding user to group: {e}")
+            instance.delete()
