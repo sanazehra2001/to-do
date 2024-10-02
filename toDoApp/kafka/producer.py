997738ae2763d55add1e0f862993dc031e2b3b25
Syeda Sana Zehra <76579833+sanazehra2001@users.noreply.github.com>
2024-10-02 23:31:27 +0500

    Moved Broker Config to .env

--- toDoApp/kafka/producer.py
+++ toDoApp/kafka/producer.py
@@ -1,9 +1,10 @@
 from kafka import KafkaProducer
 import json
+import os
 
 def get_producer():
     return KafkaProducer(
-        bootstrap_servers=['kafka:9093'],
+        bootstrap_servers=[os.getenv('KAFKA_BOOTSTRAP_SERVER')],
         value_serializer=lambda v: json.dumps(v, default=str).encode('utf-8')
     )
 
